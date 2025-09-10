7890910a1db01e815c84ff4508fc332dd8430318
CShah18 <chirageng.webforest@gmail.com>
2025-09-10 22:16:31 +0530

    Added changes

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,17 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
+const isProduction = process.env.NODE_ENV === 'production';
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  ...(isProduction && {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // use true with proper CA certs
+      },
+    },
+  }),
 });
 
 const Group = require('./group')(sequelize, DataTypes);
